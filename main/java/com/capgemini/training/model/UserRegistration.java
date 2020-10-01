321fa66a5d89901dff40fcbb1a0f60d11d48e857
Mohana Kavya K <kkunisettymohana@student.nitw.ac.in>
2020-10-01 12:02:17 +0530

    Added : Valid First Name for User is Added

--- main/java/com/capgemini/training/model/UserRegistration.java
+++ main/java/com/capgemini/training/model/UserRegistration.java
@@ -1,12 +1,33 @@
 package com.capgemini.training.model;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
 
 	public static void main(String[] args) {
+		boolean match = false;
 		
 		// Display Message
 		System.out.println("User Registration");
-
+		
+		Scanner sc = new Scanner(System.in);
+		do {
+		System.out.println("Enter Your First Name for User Registration :");
+		String firstName = sc.next();
+		
+		String patternFirstName = "^[A-Z]{1}[a-zA-z]{2,}$";
+		
+		Pattern pattern = Pattern.compile(patternFirstName);
+		Matcher m = pattern.matcher(firstName);
+		
+		if(m.matches()) 
+			match = true;
+		else
+			System.out.println("Invalid First Name. Try Again");
+		}while(match == false);
+		
 	}
 
 }
